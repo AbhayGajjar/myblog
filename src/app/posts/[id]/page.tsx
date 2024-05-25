d20474b6d2901d75f9e4f988dc8939c867960090
AbhayGajjar <abhaymgajjar@gmail.com>
2024-05-25 11:22:01 +0530

    .

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,29 +1,26 @@
-
 import UpVoteBtn from '@/components/upvote-btn';
 import prisma from '@/lib/db';
-import React from 'react'
+import React from 'react';
 import NotFound from './not-found';
 
-const page = async ({ params }: { params: { id: string } }) => {
+const Page = async ({ params }: { params: { id: string } }) => {
     const post = await prisma.post.findUnique({
         where: {
-            id: parseInt(params.id),
+            id: parseInt(params.id, 10),
         },
     });
-    if (!post) {
-        NotFound();
-    }
 
+    if (!post) {
+        return <NotFound />;
+    }
 
     return (
         <main className='px-7 pt-24 text-center'>
             <h1 className='text-5xl font-semibold mb-6'>{post.title}</h1>
             <p className='max-w-[700px] mx-auto'>{post.body}</p>
-
             <UpVoteBtn />
-
         </main>
-    )
-}
+    );
+};
 
-export default page
+export default Page;
